744c534829b5f925d46906b66713817db4dba0ec
karmdesai <krd37@njit.edu>
2021-05-13 00:27:45 -0400

    Added generateTable.js

---
+++ src/js/generateTable.js
@@ -0,0 +1,12 @@
+/* global document */
+export default function generateTable(table, data) {
+    for (const element of data) {
+        const row = table.insertRow();
+        console.log(element);
+        for (const key in element) {
+            const cell = row.insertCell();
+            const text = document.createTextNode(element[key]);
+            cell.appendChild(text);
+        }
+    }
+}
